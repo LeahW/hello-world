f900695ed34e5e4139a0bd68bc0b5aaf4a26cb1f
Leah Williamson <leahelizabethwilliamson@gmail.com>
2015-02-11 23:29:05 -0500

    Create Objects I.js

---
+++ Objects I.js
@@ -0,0 +1,13 @@
+var james = {
+    job:"programmer",                                                               // literal notation
+    married:false
+
+    // add properties to this object!
+    
+    
+};
+
+function Person(job, married) {
+    this.job = job;                                                                 // Constructor notation (creates Class)
+    this.married = married;
+}
